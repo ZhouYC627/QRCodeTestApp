9c1bf93014413247ef69513f786019c56497e681
ZhouYC627 <zhouycsf@gmail.com>
2017-02-20 22:07:52 +0800

    create thread to load image

--- app/src/main/java/nju/wujianchao/testapp/MyActivity.java
+++ app/src/main/java/nju/wujianchao/testapp/MyActivity.java
@@ -15,6 +15,8 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -28,13 +30,14 @@ public class MyActivity extends AppCompatActivity {
     private AnimationDrawable animationDrawable = null;
 
     //播放图片数目
-    int resCounts=50;
+    int resCounts=216;
 
     //图片资源编号数组
     int []res=new int[resCounts];
 
     //图片位图对象数组
-    Bitmap []bitmaps=new Bitmap[resCounts];
+    //Bitmap []bitmaps=new Bitmap[resCounts];
+    Queue<Bitmap> bitmaps = new LinkedList<Bitmap>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,39 +55,18 @@ public class MyActivity extends AppCompatActivity {
         animationDrawable = new AnimationDrawable();
 
         for(int i=0;i<resCounts;i++){
-            int id = getResources().getIdentifier("b"+i, "drawable", "nju.wujianchao.testapp");
+            int id = getResources().getIdentifier("c"+i, "drawable", "nju.wujianchao.testapp");
             res[i]=id;
         }
 
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inPurgeable=true;
-        options.inInputShareable=true;
-        //设置该属性可获得图片的长宽等信息，但是避免了不必要的提前加载动画
-        options.inJustDecodeBounds=false;
-        InputStream inputStream=null;
+        Runnable loadImg = new LoadImg();
+        Thread thread1 = new Thread(loadImg);
+        thread1.run();
 
-        for(int i=0;i<resCounts;i++){
-            inputStream=getResources().openRawResource(res[i]);
-            Bitmap bitmap =BitmapFactory.decodeStream(inputStream, null, options);
-            bitmaps[i]=bitmap;
-        }
-
-
-        for(int i=0;i<resCounts;i++){
+        while (!bitmaps.isEmpty()){
             //添加每一帧图像，第二个参数为帧间时间间隔(毫秒)
-            animationDrawable.addFrame(new BitmapDrawable(bitmaps[i]),0);
+            animationDrawable.addFrame(new BitmapDrawable(bitmaps.remove()), 33);
         }
-
-
-
-//        for(int i =0;i<30;i++){
-//            //第一个 就是我们的资源名称(图片名)
-//            //第二个 就是我们存放图片的文件夹drawable
-//            //第三个 包名也可以用Context的getPackageName返回应用程序的包名
-//            int id = getResources().getIdentifier( "a"+i, "drawable", "nju.wujianchao.testapp");
-//            System.out.println("ID:" + i);
-//            animationDrawable.addFrame(ContextCompat.getDrawable(this,id),0);
-//        }
 
         //设置是否循环播放，false为循环
         animationDrawable.setOneShot(true);
@@ -93,6 +75,25 @@ public class MyActivity extends AppCompatActivity {
 
     }
 
+
+    private class LoadImg implements Runnable{
+        @Override
+        public void run() {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPurgeable=true;
+            options.inInputShareable=true;
+            //设置该属性可获得图片的长宽等信息，但是避免了不必要的提前加载动画
+            options.inJustDecodeBounds=false;
+            InputStream inputStream=null;
+
+            for(int i=0;i<resCounts;i++){
+                inputStream = getResources().openRawResource(res[i]);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream, null, options);
+                bitmaps.offer(bitmap);
+            }
+
+        }
+    }
 
     //start stop 按钮监听函数
     private class StartListener implements View.OnClickListener {
